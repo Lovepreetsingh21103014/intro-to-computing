1de1af17ba70374d7f9c0f74d8e2de2824a43402
Lovepreetsingh21103014 <96562227+Lovepreetsingh21103014@users.noreply.github.com>
2022-01-08 14:16:42 +0530

    Add files via upload

---
+++ sol.py
@@ -0,0 +1,28 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================= RESTART: C:\Users\khush\Desktop\lovepreet1.py ================
+enter the first number32
+enter the second number22
+enter the third number11
+the average of three numbers is : 
+21.666666666666668
+enter your gross salary=300000
+number of dependented on your family=3
+your income tax is
+56200.0
+enter your name :lovepreet singh
+gender(use M(male),F(female),U(unknown)):M
+male
+enter your sid:21103014
+enter your course name:cse
+enter your cgpa:10
+('21103014', 'lovepreet singh', 'M', 'cse', 10.0)
+enter the marks of student:22
+enter the marks of student:33
+enter the marks of student:25
+enter the marks of student:26
+enter the marks of student:24
+['22', '24', '25', '26', '33']
+part a of question= ['Red', 'Green', 'White', 'Pink', 'Yellow']
+partb of question= ['Red', 'Green', 'White', 'Purple', 'Purple', 'Yellow']
