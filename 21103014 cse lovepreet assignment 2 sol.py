5298e40231e63252cfe488c65e25a2ae6143345d
Lovepreetsingh21103014 <96562227+Lovepreetsingh21103014@users.noreply.github.com>
2022-01-21 17:09:51 +0530

    Add files via upload

--- 21103014 cse lovepreet assignment 2 sol.py
+++ 21103014 cse lovepreet assignment 2 sol.py
@@ -1,7 +1,7 @@
 Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 
-================ RESTART: C:/Users/khush/Desktop/assignment2.py ================
+= RESTART: C:/Users/khush/Desktop/comp pratical/2/21103014   cse lovepreet assignment  2.py
 que 1
 part a
 lenght of string is 35
@@ -16,28 +16,32 @@ index of substring 'a' is 10
 part f
 Pythonisacasesensitivelanguage
 que 2
-enter your name:lovepreet singh
-enter your SID:21103014
-enter your department name:cse
-enter your CGPA:9.9
-('Hey', 'lovepreet singh', 'Here!   My sid is  ', 21103014, '  I am from ', 'cse', ' and my CGPA is ', 9.9)
+Please enter your name: lovepreet singh
+Enter SID: 21103014
+Enter department name: cse
+Enter CGPA: 9.9
+
+
+Hey, lovepreet singh Here!
+     My SID is 21103014
+     I am from cse department and my CGPA is 9.9
 que3
-a$b = 8
+a&b = 8
 a|b+= 58
 a**b= 303305489096114176
 left sift a by 2bits= 224 left sift b by 2 bits = 2
 right sift a by 2 bits= 14
 right sift b by 4 bits= 0
 que 4
-enter the first number:15
-enter the second number:14
-enter the third number:12
-largest number is  15.0
+Enter number 1: 33.6
+Enter number 2: 44.2
+Enter number 3: 33.7
+The greatest value is: 44.2
 ques 5
 Enter the string: my name is lovepreet singh
 yes,word name is present
 ques 6
-length of the one side= 4
-length of second side =3
-length of the third side =5
-yes, triangle can be formes
+length of the one side= 12.3
+length of second side =23.4
+length of the third side =77.2
+no, triangle cant be formed
