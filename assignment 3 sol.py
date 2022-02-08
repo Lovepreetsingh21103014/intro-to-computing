6fc8349a2c141a5d532f48bb01497fe045fdaaad
Lovepreetsingh21103014 <96562227+Lovepreetsingh21103014@users.noreply.github.com>
2022-02-08 11:05:53 +0530

    Add files via upload

---
+++ assignment 3 sol.py
@@ -0,0 +1,72 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:\Users\khush\Desktop\assignment3.py ================
+que 1
+{'my': 1, 'name': 1, 'is': 2, 'lovepreet.': 1, 'Currently': 1, 'i': 1, 'am': 1, 'doing': 1, 'btech': 1, 'in': 1, 'cse': 1, 'from': 1, 'pec': 1, 'chandigarh': 1}
+
+que2
+Input a year: 1999
+Input a month [1-12]: 2
+Input a day [1-31]: 28
+The next date is [dd-mm-yyyy] 1-3-1999.
+
+que3
+The list is
+[3, 9, 10]
+The resultant tuple is :
+[(3, 9), (9, 81), (10, 100)]
+
+que4
+Enter your grade point(from 4.0 to 10.0):9
+your grade is A and excelent performance 
+
+que5
+ABCDEFGHIJK
+ABCDEFGHI
+ABCDEFG
+ABCDE
+ABC
+A
+
+que6
+ENTER YOUR NAME OR PRESS N TO STOP Lovepreet Singh
+ENTER YOUR SID OR PRESS N TO STOP 21103014
+ENTER YOUR NAME OR PRESS N TO STOP Virat Kohli
+ENTER YOUR SID OR PRESS N TO STOP 18
+ENTER YOUR NAME OR PRESS N TO STOP Rohit Sharma
+ENTER YOUR SID OR PRESS N TO STOP 45
+ENTER YOUR NAME OR PRESS N TO STOP MS Dhoni
+ENTER YOUR SID OR PRESS N TO STOP 07
+ENTER YOUR NAME OR PRESS N TO STOP N
+part a:  {21103014: 'Lovepreet Singh', 18: 'Virat Kohli', 45: 'Rohit Sharma', 7: 'MS Dhoni'}
+part b: {21103014: 'Lovepreet Singh', 7: 'MS Dhoni', 45: 'Rohit Sharma', 18: 'Virat Kohli'}
+part c: {7: 'MS Dhoni', 18: 'Virat Kohli', 45: 'Rohit Sharma', 21103014: 'Lovepreet Singh'}
+part d: Lovepreet Singh
+
+
+
+que 7
+ENTER THE NUMBER OF TERMS IN THE SERIES: 10
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+AVERAGE OF RESULTANT FIBONACCI SERIES IS 8.8
+
+que 8
+set 1= {1, 2, 3, 4, 5}
+set2 = {8, 2, 4, 6}
+set3 = {1, 17, 5, 9, 13}
+part a )set of all the element present in both set 1 and set 2 but not in both are = {1, 3, 5, 6, 8}
+part b)set of all elements that are not in only one of three sets=  {3, 6, 8, 9, 13, 17}
+part c)set of elements that are in exactly in two of the sets= {1, 2, 4, 5}
+part d)set of all integers in the range of 1 to 10 that are not in set 1= {6, 7, 8, 9, 10}
+part e)set of all integers in the range 1 to 10 that are not in set1, set2, set3+ {10, 7}
