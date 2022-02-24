baad6ff30432abccbbfbb5398652b20a8a67bc67
Lovepreetsingh21103014 <96562227+Lovepreetsingh21103014@users.noreply.github.com>
2022-02-24 22:15:56 +0530

    Add files via upload

---
+++ assing4sol.py
@@ -0,0 +1,65 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+================ RESTART: C:\Users\khush\Desktop\assignment4.py ================
+que1
+number of disks for tower of honaoi:3
+Move disk 1 from source A to destination B
+Move disk 2 from source A to destination C
+Move disk 1 from source B to destination C
+Move disk 3 from source A to destination B
+Move disk 1 from source C to destination A
+Move disk 2 from source C to destination B
+Move disk 1 from source A to destination B
+
+que2
+enter the number of rows for pascal's triagle:10
+           1 
+          1 1 
+         1 2 1 
+        1 3 3 1 
+       1 4 6 4 1 
+      1 5 10 10 5 1 
+     1 6 15 20 15 6 1 
+    1 7 21 35 35 21 7 1 
+   1 8 28 56 70 56 28 8 1 
+  1 9 36 84 126 126 84 36 9 1 
+que3
+enter the first number:8
+enter the second number:2
+quotient of 8 and 2 is 4 
+remaider between 8 and 2 is 0 
+part a
+ Checking if the quotient and remainder is a callable value or not.
+False
+False
+part b
+ The reminder is zero
+part c
+ Filtered out numbers that are greater than 4 are : [8, 5, 9, 5, 10, 6]
+part d
+ Set: {5, 6, 8, 9, 10}
+part e
+ Immutable set: frozenset({5, 6, 8, 9, 10})
+part f
+ Hash value of the max value from the above set: 10
+
+que 4 
+lovepreet 21103014
+choota bheem  18 45 10 07
+object deleted
+object deleted
+
+que 5
+Mehak 40000
+Ashok 50000
+Viren 60000
+part a
+ The updated salary of Mehak is 70000
+part b
+Record of viren deleted
+que 6
+Enter the word: ram
+Enter a new meaningful word using the exact same letters to test your friendship: mar
+Does the word makes sense?(y/Y or n/N )n
+The word doesn't have a meaning, friendship is not true
